0062cfef59795159b6fb9a8c17a85a9269214f15
Hatem <hatem.deux@gmail.com>
2021-01-21 19:33:46 -0500

    inits a social media container component

---
+++ src/components/SocialMediaList.js
@@ -0,0 +1,11 @@
+import SocialMediaListItems from './SocialMediaListItems'
+
+export default function SocialMediaList () {
+  return (
+    <ul>
+      <SocialMediaListItems name='in'/>
+      <SocialMediaListItems name='gh'/>
+
+    </ul>
+  )
+}
