17d0939f8e10c663d32f689f8ef81319caa91c4f
Hatem <hatem.deux@gmail.com>
2021-01-21 19:30:56 -0500

    inits basic Navbar container component

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import NavBarItem from './NavBarItem';
+
+
+export default function NavBar () {
+  return (
+    <nav>
+      nav
+      <NavBarItem name='home'/>
+      <NavBarItem name='about'/>
+      <NavBarItem name='skills'/>
+      <NavBarItem name='my work'/>
+      <NavBarItem name='contact'/>
+    </nav>
+  )
+}
