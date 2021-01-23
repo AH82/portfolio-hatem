bf350beecaf912f9fe947dca26b048c0a82c9944
Hatem <hatem.deux@gmail.com>
2021-01-23 17:42:07 -0500

    adds new prop 'route' to children; renames prop 'name' to 'tabTitle'; removes text placeholder

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import NavBarItem from './NavBarItem';
 import './NavBar.css';
 
@@ -5,12 +7,11 @@ import './NavBar.css';
 export default function NavBar () {
   return (
     <nav className='navbar'>
-      nav
-      <NavBarItem name='home'/>
-      <NavBarItem name='about'/>
-      <NavBarItem name='skills'/>
-      <NavBarItem name='my work'/>
-      <NavBarItem name='contact'/>
+      <NavBarItem tabTitle='home' route='/'/>
+      <NavBarItem tabTitle='about' route='/about'/>
+      <NavBarItem tabTitle='skills' route='/skills'/>
+      <NavBarItem tabTitle='my work' route='/portfolio'/>
+      <NavBarItem tabTitle='contact' route='/contact'/>
     </nav>
   )
 }
