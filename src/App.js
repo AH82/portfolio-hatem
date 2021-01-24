0c5b8171dd94c5d132370bb696a85ff8e7496222
Hatem <hatem.deux@gmail.com>
2021-01-23 20:40:29 -0500

    imports & routes to  components: Home, About, Skills, Portfolio, Contact; + react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
+/* libraries */
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+/* components */
+import Header from './components/Header/index';
+import Home   from './components/Home';
+import About  from './components/About';
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Portfolio from "./components/Portfolio";
+/* css */
 import './App.css';
-import Header from './components/Header';
 
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
 
-      <Header/>
+      <BrowserRouter>
+
+        <Header/>
+
+        <Switch>
+          <Route path='/about'      exact component={About} />
+          <Route path='/Skills'     exact component={Skills} />
+          <Route path='/Contact'    exact component={Contact} />
+          <Route path='/Portfolio'  exact component={Portfolio} />
+          <Route path='/'           exact component={Home} />
+        </Switch>
+
+      </BrowserRouter>
 
 
     </div>
   );
 }
 
-export default App;
+// export default App;
