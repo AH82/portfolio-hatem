c26c96028c86747bfd855e9f5d8873faddf907ca
Hatem <hatem.deux@gmail.com>
2021-01-30 07:02:38 -0500

    refactors on mock data on a basic level

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+const title = [
+  `Bonjour/Hi,`,
+  `I'm Hatem,`,
+  `full stack web developer.`
+]
+const subTitle = `
+junior, fresh, entry-level.
+`;
+
 export default function Home() {
   return (
-    <div style={{color: 'white'}}>
-      Honey! I'm Home!
+
+    <div >
+      <h1>
+    {title.map(sentence => <>{sentence} <br/></>)}
+    </h1>
+    <p>{subTitle}</p>
     </div>
   )
 }
