4aadb90b8f019b3f735c28d797f40dfb69d271da
Hatem <hatem.deux@gmail.com>
2021-01-30 07:00:43 -0500

    replaces text with icons, adds mock data, and refactors accordingly

--- src/components/Header/SocialMediaList.js
+++ src/components/Header/SocialMediaList.js
@@ -1,11 +1,19 @@
 import SocialMediaListItems from './SocialMediaListItems'
 
-export default function SocialMediaList () {
-  return (
-    <ul>
-      <SocialMediaListItems name='in'/>
-      <SocialMediaListItems name='gh'/>
+export default function SocialMediaList (props) {
 
-    </ul>
+  const socialMedia = [
+    { name: "linkedIn", url: "https://www.linkedin.com/in/hatem-deux/" },
+    { name: "gitHub", url: "https://github.com/AH82" }
+  ];
+
+  return (
+    <div className='social-media'>
+      {
+        socialMedia.map( (elem) => (
+          <SocialMediaListItems name={elem.name} url={elem.url}/>
+        ))
+      }
+    </div>
   )
 }
--- src/components/Header/SocialMediaListItems.js
+++ src/components/Header/SocialMediaListItems.js
@@ -1,8 +1,21 @@
+import React from "react";
+// import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { iconSelector } from "../../helpers/iconSelector";
+
 export default function SocialMediaListItems (props) {
-  const { name } = props;
+  const { name, url } = props;
+
+  // const iconSelector = function (icon) {
+  //   switch (icon.toLowerCase()) {
+  //     case 'github': return <FaGithub/>;
+  //     case 'linkedin': return <FaLinkedinIn/>;
+  //   }
+  // }
   return (
-    <li>
-      {name}
-    </li>
+    <div>
+      <a href={url} target="_blank" rel='noreferrer' >
+        { iconSelector(name) }
+      </a>
+    </div>
   )
 }
