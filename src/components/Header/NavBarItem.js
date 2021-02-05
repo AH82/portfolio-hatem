debf76233a7b7ac4a46623909a1bd3743bf6c1ae
Hatem <hatem.deux@gmail.com>
2021-02-05 08:22:56 -0500

    upgrades navbar with icons, which changes to text on hovering.

--- src/components/Header/NavBarItem.js
+++ src/components/Header/NavBarItem.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react'
+import { iconSelector } from '../../helpers/iconSelector'
 import { NavLink } from "react-router-dom";
 
 import './NavBarItem.css';
@@ -5,9 +7,17 @@ import './NavBarItem.css';
 
 export default function NavBarItem (props) {
   const { tabTitle, route } = props;
+  const [isHovering, setIsHovering] = useState(false);
+
   return (
-    <NavLink to={route} exact className='nav-btn' activeClassName='nav-btn-current'>
-        {tabTitle}
+    <NavLink to={route} exact 
+    className='nav-btn' 
+    activeClassName='nav-btn-current'
+    onMouseEnter={()=> setIsHovering(true)}
+    onMouseLeave={()=> setIsHovering(false)}
+    >
+    {!isHovering && iconSelector(tabTitle)}
+    {isHovering && tabTitle}
     </NavLink>
   )
 };
