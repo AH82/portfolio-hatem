b33877c36acb6b47717934119b731e64e2de812b
Hatem <hatem.deux@gmail.com>
2021-02-08 06:31:34 -0500

    adds a language State and a method for changing it.

--- src/hooks/useAppData.js
+++ src/hooks/useAppData.js
@@ -17,28 +17,63 @@ import contacts from '../db/contacts.json'
 
 
 export default function useAppData(initial) {
+  const defaultLanguage = "en";
+  const [ currentLanguage, setCurrentLanguage ] = useState("en");
+  
 
-const [ dataState, setDataState ] = useState(
-  {
-    locale,
-    socialMedia,
-    home,
-    about,
-    education,
-    employment,
-    skills,
-    skillSet,
-    projectsData,
-    contacts
-  }
-) 
+
+  const [ dataState, setDataState ] = useState(
+    {
+      locale,
+      socialMedia,
+      home,
+      about,
+      // about: about[currentLanguage],
+      education,
+      employment,
+      skills,
+      // skills: skills[currentLanguage],
+      skillSet,
+      projectsData,
+      contacts
+    }
+  );
+
+  function changeLanguage(lang) {
+    console.log("requested language: ", lang);
+
+    setCurrentLanguage(lang.toLowerCase());
+    console.log("Current language is: ", currentLanguage);
+    // setDataState(
+    //   (prev) => ({
+    //     ...prev, 
+    //     // about: about[currentLanguage],
+    //     skills: skills[currentLanguage]
+    //   })
+    //   )
+    };
+    console.log("Current language is: ", currentLanguage);
+    
+/*     // verfies data in selected language, else defaults to English.
+    const verifyLocale = function (langsContainer, lang) {
+      // if empty array
+      if (Array.isArray(langsContainer[lang]) && !langsContainer[lang].length) {
+        return "en";
+      } else {
+        // if empty string
+        return langsContainer[lang] ? lang : "en";
+      }
+    }; */
+
 
 
 
 
   return (
     {
-      dataState
+      dataState,
+      currentLanguage,
+      changeLanguage
     }
   )
 }
