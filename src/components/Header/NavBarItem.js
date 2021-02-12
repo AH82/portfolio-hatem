efc9e68d206963d2e20d4c88567599da2e6d3567
Hatem <hatem.deux@gmail.com>
2021-02-12 10:06:31 -0500

    deletes NavBarItem.css & removes its import from NavBarItem.js

--- src/components/Header/NavBarItem.js
+++ src/components/Header/NavBarItem.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react'
 import { iconSelector } from '../../helpers/iconSelector'
 import { NavLink } from "react-router-dom";
 
-import './NavBarItem.css';
-
 
 export default function NavBarItem (props) {
   const { tabKey, localeNavBtns, lang, route } = props;
