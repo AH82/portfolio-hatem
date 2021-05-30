4411cee2042a8ee9b8ff449cf2c1b172f33041c7
Hatem <hatem.deux@gmail.com>
2021-05-30 12:30:14 -0400

    adds message content

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -4,11 +4,21 @@ export default function Contacts(props) {
 
   const { localePageHeadings, lang } = props;
 
+  let message = "";
+  switch(lang) {
+    case "fr": message = `Pour le moment, vous pouvez me contacter sur mon compte de LinkedIn.`
+    break;
+    // case "en": message = `For the time being, you can contact me on my LinkedIn account.`
+    // break;
+    default: message = `For the time being, you can contact me on my LinkedIn account.`
+  };
+  
   return (
     <div className='main-page'>
       <h1>
         {localePageHeadings[lang].contact}
       </h1>
+      <p>{message}</p>
     </div>
   )
 }
