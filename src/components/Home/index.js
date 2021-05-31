41653d0ee01807bf0ad58dc4e075cabaa1f10669
Hatem <hatem.deux@gmail.com>
2021-05-31 18:08:34 -0400

    refactors and adds FR

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,22 +1,52 @@
 import React from 'react';
+import HatemDeuxAllFace from '../HatemDeuxAllFace_svg.js'
 
-const title = [
-  `Bonjour, Hi!,`,
-  `I am Hatem,`,
-  `A Full-Stack Web-Developer.`
-]
-const subTitle = `
-Junior, Fresh, Entry-Level.
-`;
+
 
 export default function Home(props) {
+  const {    
+    changeLanguage, 
+    lang 
+  } = props;
+
+  const title = {
+    en: [
+      `Bonjour / Hi ! .. I am`,
+      // `I am `,
+      `Hatem`,
+      `A Full-Stack Web-Developer.`
+    ],
+    fr: [
+      `Bonjour/Hi!,`,
+      `Je m'appelle `,
+      `Hatem`,
+      `Un développeur Web Full-Stack.`
+    ]
+  }
+  const subTitle = {
+    en: `
+    Junior, Fresh, Entry-Level.
+    `,
+    fr:`
+    Junior, Frais, Débutant.
+    `
+  }
+  // Junior
+  // Frais
+  // premier échelon, débutants
+  
+
   return (
 
-    <div className='main-page home' >
-      <h1>
-    {title.map(sentence => <>{sentence} <br/></>)}
-    </h1>
-    <p>{subTitle}</p>
+    <div key={'home-'+lang} className='main-page home col-2' >
+      <div>
+        <h1 key={'home-title-'+lang}>
+          {title[lang].map(sentence => 
+          sentence === 'Hatem' ? <span className='hatem'>{sentence}</span> :<><br/>{sentence}</>)}
+        </h1>
+        <p key={'subtitle-'+lang} className='subtitle' >{subTitle[lang]}</p>
+      </div>
+        <HatemDeuxAllFace/>
     </div>
   )
 }
