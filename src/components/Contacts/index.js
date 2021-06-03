25c662167921c38818ab6c0ef01bd52fa74eb6bf
Hatem <hatem.deux@gmail.com>
2021-06-03 00:12:14 -0400

    refactors, inserts link into text string

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -4,21 +4,38 @@ export default function Contacts(props) {
 
   const { localePageHeadings, lang } = props;
 
-  let message = "";
-  switch(lang) {
-    case "fr": message = `Pour le moment, vous pouvez me contacter sur mon compte de LinkedIn.`
-    break;
-    // case "en": message = `For the time being, you can contact me on my LinkedIn account.`
-    // break;
-    default: message = `For the time being, you can contact me on my LinkedIn account.`
-  };
+  // const linkedin = () => (<a href='https://www.linkedin.com/in/hatem-deux/'>LinkedIn</a>)
+  const content = {
+    fr: {
+      // message: [`Pour le moment, vous pouvez me contacter sur mon compte de `, linkedin(), `.`]
+      message: `Pour le moment, vous pouvez me contacter sur mon compte de LinkedIn.`
+    },
+    en: {
+      // message: [`For the time being, you can contact me on my `, linkedin(), ` account.`]
+      message: `For the time being, you can contact me on my LinkedIn account.`
+    }
+  }
   
   return (
-    <div key={'contacts-'+lang} className='main-page'>
-      <h1 key={'contact-heading-'+lang}>
+    <div 
+      key={'contact-'+lang} 
+      className='main-page'
+      >
+
+      <h1 
+        key={'contact-heading-'+lang}
+        >
         {localePageHeadings[lang].contact}
       </h1>
-      <p key={'p'+lang}>{message}</p>
+
+      <p 
+        key={'contact-content-'+lang}
+        >
+        {content[lang].message.split(/linkedin/i)[0]}
+        <a href='https://www.linkedin.com/in/hatem-deux/'>LinkedIn</a>
+        {content[lang].message.split(/linkedin/i)[1]}
+      </p>
+
     </div>
   )
 }
