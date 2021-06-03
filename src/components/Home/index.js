43772eb2d102930e387a8dd7ef362cc6bdbe3373
Hatem <hatem.deux@gmail.com>
2021-06-03 03:40:18 -0400

    refactors the component & data content

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,52 +1,76 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import HatemDeuxAllFace from '../HatemDeuxAllFace_svg.js'
 
 
-
 export default function Home(props) {
-  const {    
-    changeLanguage, 
-    lang 
-  } = props;
+  const { changeLanguage, lang } = props;
+  // const [ hatem, setHatem ] = useState("");
 
-  const title = {
-    en: [
-      `Bonjour / Hi ! .. I am`,
-      // `I am `,
-      `Hatem`,
-      `A Full-Stack Web-Developer.`
-    ],
-    fr: [
-      `Bonjour/Hi!,`,
-      `Je m'appelle `,
-      `Hatem`,
-      `Un développeur Web Full-Stack.`
-    ]
-  }
-  const subTitle = {
-    en: `
-    Junior, Fresh, Entry-Level.
-    `,
-    fr:`
-    Junior, Frais, Débutant.
-    `
-  }
-  // Junior
-  // Frais
-  // premier échelon, débutants
-  
+  // Data
+  const homeMessage = {
+    en: {
+      greeting: `Bonjour / Hi !`,
+      intro: `I am`,
+      name: `Hatem`,
+      jobTitle: `A Full-Stack Web-Developer.`,
+      tagline: `Junior, Fresh, Entry-Level.`
+    },
+    fr: {
+      greeting: `Bonjour / Hi !`,
+      intro: `Je m'appelle`,
+      name: `Hatem`,
+      jobTitle: `Un développeur Web Full-Stack`,
+      tagline: `Junior, Frais, Débutant.` // Débutant or Premier Échelon
+    },
+    // ww = world wide // just as a template
+    ww: {
+      greeting: ``,
+      intro: ``,
+      name: ``,
+      jobTitle: ``,
+      tagline: ``
+    }
+  };
+ 
+/*  
+  // NO LONGER USEFUL, KEPT FOR REFERENCE ONLY
+  useEffect(() => {
+    setHatem("");
+    [...'Hatem'].map( (char, index) => 
+     setTimeout(() => {
+       setHatem ((prev)=> prev+char )
+      }, 1000 + ( index*100))
+    )
+    
+    return () => {
+    <span>{hatem}</span>
+    }
+  }, [lang])
 
+ */
   return (
 
-    <div key={'home-'+lang} className='main-page home col-2' >
-      <div>
-        <h1 key={'home-title-'+lang}>
-          {title[lang].map(sentence => 
-          sentence === 'Hatem' ? <span className='hatem'>{sentence}</span> :<><br/>{sentence}</>)}
+    <div  className='main-page home col-2' >
+      <div key={'home-'+lang}>
+
+        <h1 
+          key={'home-greeting-'+lang}
+          >
+          <div>{homeMessage[lang].greeting + " .. "}</div>
+          <div>{homeMessage[lang].intro} <span className='hatem'>{homeMessage[lang].name}</span></div>
+          
+          <div>{homeMessage[lang].jobTitle}</div>
         </h1>
-        <p key={'subtitle-'+lang} className='subtitle' >{subTitle[lang]}</p>
+
+        <p 
+          key={'tagline-'+lang} 
+          className='tagline' 
+          >
+          {homeMessage[lang].tagline}
+        </p>
+
       </div>
-        <HatemDeuxAllFace/>
+      <HatemDeuxAllFace/>
     </div>
   )
 }
